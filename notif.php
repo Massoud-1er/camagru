f38b580db1ebd971f7b2fd8c42fe3a616a9c07fa
Massoud MOVAHHED <mmovahhe@e3r11p15.42.fr>
2019-01-22 22:30:11 +0100

    avec mail pour notif

---
+++ notif.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+
+$msg = "une de vos photos a été commenté, allez vite vous connecter pour le voir, connard";
+$msg = wordwrap($msg, 35, "\r\n");
+$header = "From: Massoud MOVAHHED <mmovahhe@e3r11p15.42.fr>"."\n";
+$header.= "Reply-to: \"mass\" <monsieurmassoud@gmail.fr>"."\n";
+$header.= "MIME-Version: 1.0"."\n";
+$sub = "fricouille";
+
+mail("monsieurmassoud@gmail.com", $sub, $msg, $header);
+
+
+?>
