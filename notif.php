0612f1ce81b3146c89d9404d3b26f9939cc74e42
Massoud MOVAHHED <mmovahhe@e3r11p10.42.fr>
2019-01-23 14:13:02 +0100

    avec upload

--- notif.php
+++ notif.php
@@ -3,7 +3,7 @@
 
 $msg = "une de vos photos a été commenté, allez vite vous connecter pour le voir, connard";
 $msg = wordwrap($msg, 35, "\r\n");
-$header = "From: Massoud MOVAHHED <mmovahhe@e3r11p15.42.fr>"."\n";
+$header = "From: mmovahhe  <mmovahhe@e3r11p15.42.fr>"."\n";
 $header.= "Reply-to: \"mass\" <monsieurmassoud@gmail.fr>"."\n";
 $header.= "MIME-Version: 1.0"."\n";
 $sub = "fricouille";
