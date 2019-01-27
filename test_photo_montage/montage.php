01fafc2fbe43faca186e059b4a9bc6403e2eba14
Massoud MOVAHHED <mmovahhe@e3r11p9.42.fr>
2019-01-27 14:29:35 +0100

    avec le montage photo fonctionnelle

--- test_photo_montage/montage.php
+++ test_photo_montage/montage.php
@@ -1,36 +1,43 @@
 #!/usr/bin/php
+
 <?php
 
-    $image = imagecreatefromjpeg ('leonard.jpg');
-    $filter = imagecreatefrompng('https://raw.githubusercontent.com/wickedpool/Camagru-42/master/images/trash.png');
+function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct){ 
+    if(!isset($pct)){ 
+        return false; 
+    } 
+    $pct /= 100; 
+    $w = imagesx( $src_im ); 
+    $h = imagesy( $src_im ); 
+    imagealphablending( $src_im, false ); 
+    $minalpha = 127; 
+    for( $x = 0; $x < $w; $x++ ) 
+    for( $y = 0; $y < $h; $y++ ){ 
+        $alpha = ( imagecolorat( $src_im, $x, $y ) >> 24 ) & 0xFF; 
+        if( $alpha < $minalpha ){ 
+            $minalpha = $alpha; 
+        } 
+    }
+    for( $x = 0; $x < $w; $x++ ){ 
+        for( $y = 0; $y < $h; $y++){ 
+            $colorxy = imagecolorat( $src_im, $x, $y ); 
+            $alpha = ( $colorxy >> 24 ) & 0xFF; 
+            if( $minalpha !== 127 )
+            $alpha = 127 + 127 * $pct * ( $alpha - 127 ) / ( 127 - $minalpha ); 
+            else
+            $alpha += 127 * $pct; 
+            $alphacolorxy = imagecolorallocatealpha( $src_im, ( $colorxy >> 16 ) & 0xFF, ( $colorxy >> 8 ) & 0xFF, $colorxy & 0xFF, $alpha ); 
+            if(!imagesetpixel( $src_im, $x, $y, $alphacolorxy))
+            return false; 
+        } 
+    } 
+    imagecopy($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h); 
+}
 
-    imagealphablending($filter, false);
-    imagesavealpha($filter, true);
-    
-    imagejpeg($filter, 'filter.png');
+$image = imagecreatefrompng("face.png");
+$filter = imagecreatefrompng("smiley.png");
+imagecopymerge_alpha($image, $filter, 100, 100, 0, 0, imagesx($filter), imagesy($filter), 100); 
+imagesavealpha($image, true); 
+imagepng($image, 'toto.png');
 
-    $x = imagesx($filter);
-    $y = imagesy($filter);
-    
-
-
-
-
-    // $newImg = imagecreatetruecolor($x, $y);
-
-    //  imagealphablending($newImg, false);
-    //  imagesavealpha($newImg,true);
-    //  $transparent = imagecolorallocatealpha($newImg, 255, 255, 255, 127);
-    //  imagefilledrectangle($newImg, 0, 0, $x, $y, $transparent);
-
-    // imagecopyresampled($newImg, $filter, 0, 0, 0, 0, $x, $y, $x, $y);
-
-
-
-
-
-
-    imagecopymerge($image, $filter, 0, 0, 0, 0, $x, $y, 100);
-    imagejpeg($image, 'v.png');
-    imagedestroy($image);
 ?>
