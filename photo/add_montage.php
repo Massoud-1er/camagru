2e0c1a041be37d99d628df6918b40d0d074d8f58
Massoud MOVAHHED <mmovahhe@e1r7p14.42.fr>
2019-02-05 15:30:18 +0100

    avec tout les montages, meme plusieurs a la fois

--- photo/add_montage.php
+++ photo/add_montage.php
@@ -37,12 +37,15 @@ function save_img()
         $date = date('Y-m-d', time());
         $login = $_SESSION['login'];
         $file = "uploads/".find_id().".png";
-        $query = $pdo->prepare("INSERT INTO `photos` (`photo`, `login`, `date`, `like`) VALUES ('$file', '$login', '$date', 0)");
-        try {
-            $query->execute();
-            echo "la photo a bien été mise dans la db";
-        } catch (PDOexception $e) {
-            echo "la photo n'a pas été mise dans la db";
+        if (file_exists($file))
+        {
+            $query = $pdo->prepare("INSERT INTO `photos` (`photo`, `login`, `date`, `like`) VALUES ('$file', '$login', '$date', 0)");
+            try {
+                $query->execute();
+                echo "la photo a bien été mise dans la db";
+            } catch (PDOexception $e) {
+                echo "la photo n'a pas été mise dans la db";
+            }
         }
     }
     else
